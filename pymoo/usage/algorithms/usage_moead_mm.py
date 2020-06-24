866d5a340df320d40d6e08d2ceed9a63a14955b6
Peng-YM <1048217874@qq.com>
2020-06-24 10:39:34 +0800

    Now the Evaluator supports archiving all evaluated solutions

--- pymoo/usage/algorithms/usage_moead_mm.py
+++ pymoo/usage/algorithms/usage_moead_mm.py
@@ -3,12 +3,15 @@ from pymoo.problems.multi.mmf import MMF2
 from pymoo.visualization.scatter import Scatter
 from pymoo.factory import get_reference_directions
 from pymoo.optimize import minimize
+from pymoo.model.evaluator import Evaluator
 
 
 problem = MMF2()
+uea_evaluator = Evaluator(use_archive=True)
+
 algorithm = MOEAD_MM(
     ref_dirs=get_reference_directions('energy', n_dim=problem.n_obj, n_points=100),
     sub_pop_size=4
 )
-res = minimize(problem, algorithm, termination=('n_gen', 100), verbose=True)
+res = minimize(problem, algorithm, termination=('n_gen', 200), verbose=True, evaluator=uea_evaluator)
 Scatter().add(res.X).show()
