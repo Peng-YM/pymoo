c8066ee854f50479e010bf274321863fac078493
Peng-YM <1048217874@qq.com>
2020-06-25 16:12:37 +0800

    Update the use case

--- pymoo/usage/problems/usage_distance_minimization.py
+++ pymoo/usage/problems/usage_distance_minimization.py
@@ -9,6 +9,8 @@ from pymoo.optimize import minimize
 from pymoo.problems.multi.distance_minimization import DistanceMinimization
 from pymoo.util.nds.non_dominated_sorting import NonDominatedSorting
 from pymoo.visualization.scatter import Scatter
+from pymoo.subset_selection.distance_based_subset_selection import DistanceBasedSubsetSelection
+
 
 polyhedra = np.array([
     (0, 0, 0),
@@ -43,20 +45,21 @@ res = minimize(
         ref_dirs=get_reference_directions('energy', problem.n_obj, 100),
         sub_pop_size=4
     ),
-    termination=('n_gen', 200),
-    verbose=True,
+    termination=('n_eval', 100000),
+    verbose=False,
     evaluator=uea_evaluator
 )
 
 # non-dominated-sorting
 archive = res.algorithm.evaluator.archive
-selected = NonDominatedSorting().do(archive.get("F"), only_non_dominated_front=True)
+nds = NonDominatedSorting().do(archive.get("F"), only_non_dominated_front=True)
+selected = DistanceBasedSubsetSelection(archive[nds]).do(n_select=100)
 
 # visualization
-fig = plt.figure()
 plt.rc('font', family='serif')
+fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.set(xlabel="$x_1$", ylabel="$x_2$", zlabel="$x_3$", title="Decision Space")
+
 plot = Scatter(ax=ax)
 
 # show polyhedras
@@ -68,7 +71,7 @@ for s in shifts:
     ax.plot(x, y, z, color='black')
 
     for k in range(4):
-        x = [vertices[k, 0], vertices[4, 0]]
+        x = [vertices[k, 0], vertices[4,  0]]
         y = [vertices[k, 1], vertices[4, 1]]
         z = [vertices[k, 2], vertices[4, 2]]
         ax.plot(x, y, z, color='black')
@@ -77,4 +80,6 @@ for s in shifts:
 plot.add(archive[selected].get("X"), s=30, facecolor=(1, 1, 1, 1), edgecolors='r', label="Solutions").do()
 
 ax.legend()
-plt.show()
+ax.set(xlabel="$x_1$", ylabel="$x_2$", zlabel="$x_3$", title="Decision Space")
+
+plt.savefig("res.pdf")
